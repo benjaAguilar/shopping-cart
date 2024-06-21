bd652ff01fe96f7bbb976d70e26c3e86e1fdf32a
Benja Aguilar <benjaaguilar02@gmail.com>
2024-06-21 14:33:45 -0300

    feat: home page component
    this renders the header and makes a onMount
    fetching to get the products categories.
    Also has products box to show the products on
    route changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from "react";
+import getData from "./getData";
+import { Link, Outlet } from "react-router-dom";
+
 function Home() {
+  const [sections, setSections] = useState([]);
+  const [showSections, setShowSections] = useState(false);
+
+  useEffect(() => {
+    const getCategoriesData = async () => {
+      const response = await getData(
+        "https://dummyjson.com/products/categories"
+      );
+      const sections = await response;
+      setSections(sections);
+    };
+
+    getCategoriesData();
+  }, []);
+
+  const toggleShowSections = () => {
+    setShowSections(!showSections);
+  };
+
   return (
-    <div>
-      <h1>Hello home</h1>
-      <p>you are on home</p>
-    </div>
+    <>
+      <header>
+        <button onClick={toggleShowSections}>Categories</button>
+        <input type="text" placeholder="Search Products" />
+        <h1>My Dummy Shop</h1>
+      </header>
+      {sections.length > 0 ? (
+        <>
+          <div
+            className="categories"
+            style={{ display: showSections ? "block" : "none" }}
+          >
+            <ul>
+              {sections.map((section) => {
+                return (
+                  <Link key={section.slug} to={`/${section.slug}`}>
+                    {section.name}
+                  </Link>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="productsBox">
+            <Outlet />
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
   );
 }
 
