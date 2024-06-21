bf7f08cfd27a9371136c8012d31b419a686b2217
Benja Aguilar <benjaaguilar02@gmail.com>
2024-06-21 14:30:50 -0300

    added children to main router navigation
    this makes posible to navigate between diferent
    routes (products categories)

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import ErrorPage from "./ErrorPage";
+import ProductCategory from "./ProductCategory";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/:categoriesId",
+        element: <ProductCategory />,
+      },
+    ],
   },
 ]);
 
