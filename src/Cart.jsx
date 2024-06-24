ba3f557f8913c9b30e0adccd19c1e4bd79251e8b
Benja Aguilar <benjaaguilar02@gmail.com>
2024-06-24 15:29:40 -0300

    feat: cart component
    this gets the cart from localstorage and prints
    on screen showing the products info and the total
    pay

---
+++ src/Cart.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+function Cart() {
+  const [cartProducts, setCartProducts] = useState();
+  let total = 0;
+
+  if (cartProducts) {
+    cartProducts.map((product) => {
+      total += product.price * product.quantity;
+    });
+  }
+
+  useEffect(() => {
+    const items = localStorage.getItem("cart");
+    const products = JSON.parse(items);
+
+    setCartProducts(products);
+  }, []);
+
+  function deleteItem(name) {
+    const filteredItems = cartProducts.filter(
+      (product) => product.title !== name
+    );
+
+    const stringed = JSON.stringify(filteredItems);
+
+    localStorage.setItem("cart", stringed);
+    setCartProducts(filteredItems);
+  }
+
+  return cartProducts ? (
+    <>
+      {cartProducts.map((product) => {
+        return (
+          <div key={product.title}>
+            <img src={product.image} alt="" />
+            <h3>{product.title}</h3>
+            <p>Quantity: {product.quantity}</p>
+            <p>{product.price * product.quantity}</p>
+            <button onClick={() => deleteItem(product.title)}>del</button>
+          </div>
+        );
+      })}
+
+      <h2>Total: $ {total}</h2>
+    </>
+  ) : (
+    <h2>No items yet</h2>
+  );
+}
+
+export default Cart;
